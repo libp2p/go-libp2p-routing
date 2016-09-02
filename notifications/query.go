d6feee5945d41d70294a1a57373b1852758dbcc1
George Antoniadis <george@noodles.gr>
2016-09-02 15:15:17 +0100

    Undo gx rewrite

--- notifications/query.go
+++ notifications/query.go
@@ -3,9 +3,9 @@ package notifications
 import (
 	"encoding/json"
 
-	pstore "gx/ipfs/QmSZi9ygLohBUGyHMqE5N6eToPwqcg7bZQTULeVLFu7Q6d/go-libp2p-peerstore"
-	peer "gx/ipfs/QmWtbQU15LaB5B1JC2F7TV9P4K88vD3PpA4AJrwfCjhML8/go-libp2p-peer"
-	context "gx/ipfs/QmZy2y8t9zQH2a1b8q2ZSLKp17ATuJoCNxxyMFG5qFExpt/go-net/context"
+	peer "github.com/ipfs/go-libp2p-peer"
+	pstore "github.com/ipfs/go-libp2p-peerstore"
+	context "golang.org/x/net/context"
 )
 
 const RoutingQueryKey = "RoutingQueryEvent"
